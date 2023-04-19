5c95d03591ebab8146c23d3d77f12d14e133500e
RamTsk <ramtsk24685@gmail.com>
2023-04-19 09:33:44 +0530

    added the change for configuring db on the Bean

---
+++ storeapp/src/main/java/com/mphasis/storeapp/config/DataSourceConfig.java
@@ -0,0 +1,25 @@
+package com.mphasis.storeapp.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:test");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate(getDataSource());
+    }
+}
