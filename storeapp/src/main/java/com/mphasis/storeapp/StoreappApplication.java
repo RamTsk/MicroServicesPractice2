fc63a15b99f46c508db4308c34e2adcbf7d2fd89
RamTsk <ramtsk24685@gmail.com>
2023-04-19 09:33:44 +0530

    added the change for configuring db on the Bean

--- storeapp/src/main/java/com/mphasis/storeapp/StoreappApplication.java
+++ storeapp/src/main/java/com/mphasis/storeapp/StoreappApplication.java
@@ -4,28 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class StoreappApplication implements CommandLineRunner {
 	@Autowired
-//	private JdbcTemplate jdbcTemplate = DBConfig.jdbcTemplate();
-//	private JdbcTemplate jdbcTemplate;
+	private JdbcTemplate jdbcTemplate;
 	public static void main(String[] args) {
 		SpringApplication.run(StoreappApplication.class, args);
 	}
 	
 	@Override
 	public void run(String... args) throws Exception {
-//		String sql = "INSERT INTO pc_products (id, name, pc_price) VALUES (?, ?, ?)";
-//		int result;
-//		result = jdbcTemplate.update(sql, "1001", "LG", "10345.66");
-//		result = jdbcTemplate.update(sql, "1001", "APPLE", "10599.99");
-//		result = jdbcTemplate.update(sql, "1001", "NOKIA", "12345.00");
-//		result = jdbcTemplate.update(sql, "1001", "ONIDA", "10300.00");
-//		result = jdbcTemplate.update(sql, "1001", "SAMSUNG", "10250.00");
-//		if (result > 0) {
-//			System.out.println("A new row has been inserted.");
-//		}
+		String sql = "INSERT INTO pc_products (id, name, product_price) VALUES (?, ?, ?)";
+		int result;
+		result = jdbcTemplate.update(sql, "1001", "LG", "10345.66");
+		result = jdbcTemplate.update(sql, "1002", "APPLE", "10599.99");
+		result = jdbcTemplate.update(sql, "1003", "NOKIA", "12345.00");
+		result = jdbcTemplate.update(sql, "1004", "ONIDA", "10300.00");
+		result = jdbcTemplate.update(sql, "1005", "SAMSUNG", "10250.00");
+		if (result > 0) {
+			System.out.println("A new row has been inserted.");
+		}
 	}
 }
--- storeapp/src/main/java/com/mphasis/storeapp/service/ProductServiceImpl.java
+++ storeapp/src/main/java/com/mphasis/storeapp/service/ProductServiceImpl.java
@@ -6,41 +6,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mphasis.storeapp.domain.Product;
+import com.mphasis.storeapp.repository.IProductRepository;
 
 @Service
 public class ProductServiceImpl implements IProductService {
 
-@Autowired	
-private IProductService ProductService;
-	
+	@Autowired
+	private IProductRepository repository;
+
 	@Override
 	public Product addProduct(Product product) {
-
-		return null;
+		return repository.save(product);
 	}
 
 	@Override
 	public Product updateProduct(Product product) {
-		return null;
-	 }
+		return repository.save(product);
+	}
 
 	@Override
 	public Product updateProductPrice(Integer productId, Double productPrice) {
-		return null;
+
+		Product product=repository.findById(productId).get();
+
+		product.setPrice(productPrice);
+
+		return repository.save(product);
 	}
 
 	@Override
 	public Product findProductById(Integer productId) {
-		return null;
+		// TODO Auto-generated method stub
+		return repository.findById(productId).get();
 	}
 
 	@Override
 	public List<Product> findAllProducts() {
-		return null;
+		return repository.findAll();
 	}
 
 	@Override
 	public void deleteProduct(Integer productId) {
+		repository.deleteById(productId);
 
 	}
 
